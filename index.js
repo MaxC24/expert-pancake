c334fc468c3e171acdf62a97eb80b3764a8ba459
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2017-01-06 12:26:56 -0500

    creates signup, login forms components

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const app = express();
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
 
-app.get('/*', (req, res) => {
-	res.send(path.join(__dirname, '/static/index.html'));
-})
+//express automatically looks for a index.html to send back to the client
+
+// app.get('/*', (req, res) => {
+// 	res.send(path.join(__dirname, '/static/index.html'));
+// })
 
 app.listen(3000, () =>  {
 	console.log('Server is running on port' + 3000);
