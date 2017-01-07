24a8ee69b009307c41a3c894b6d2a54ed8f07922
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2017-01-07 17:59:24 -0500

    always serves the index.html file for whatever route

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const app = express();
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
 
-//express automatically looks for a index.html to send back to the client
 
-// app.get('/*', (req, res) => {
-// 	res.send(path.join(__dirname, '/static/index.html'));
-// })
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'server/static/index.html'));
+})
 
 app.listen(3000, () =>  {
 	console.log('Server is running on port' + 3000);
