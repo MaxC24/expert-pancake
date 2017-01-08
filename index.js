093f52b70075bf1c3b72a9d7dc01eaca54fb0e12
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2017-01-08 12:02:35 -0500

    creates server routers to validate the login and signup infos

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/auth', require('./server/routes/auth'));
 
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'server/static/index.html'));
